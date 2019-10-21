292ffb7205eab72c81716d8f34b69b7ff44f546b
simcup <cheff@simcup.de>
2019-10-21 18:39:31 +0200

    structural reasons

--- ampelv2.ino
+++ ampelv2.ino
@@ -65,6 +65,17 @@ void normal_modus(){
   ampel_gelbrot.next = &ampel_gruen;
 }
 
+void es_werde_licht(Zustand *zustand){
+  Serial.printf("rot: %d, gelb: %d, gruen: %d\n",
+	zustand->led_rot,
+	zustand->led_gelb,
+	zustand->led_gruen); 
+  digitalWrite(ROTE_LAMPE, zustand->led_rot);
+  digitalWrite(GELBE_LAMPE, zustand->led_gelb);
+  digitalWrite(GRUENE_LAMPE, zustand->led_gruen);
+  delay(zustand->dauer);
+}
+
 void setup() {
   // Serial output mit baud rate 115200 initialisieren
   Serial.begin(115200);
@@ -93,16 +104,6 @@ void setup() {
   normal_modus();
 }
 
-void es_werde_licht(Zustand *zustand){
-  Serial.printf("rot: %d, gelb: %d, gruen: %d\n",
-	zustand->led_rot,
-	zustand->led_gelb,
-	zustand->led_gruen); 
-  digitalWrite(ROTE_LAMPE, zustand->led_rot);
-  digitalWrite(GELBE_LAMPE, zustand->led_gelb);
-  digitalWrite(GRUENE_LAMPE, zustand->led_gruen);
-  delay(zustand->dauer);
-}
 
 void loop() {
   es_werde_licht(jetzt);
